d036056a4710677a439c8af45f3eaf407af6d020
Constanze Meyer <constanze.xyz@gmail.com>
2022-11-24 15:11:22 +0100

    add bookmarks_controller methods and related views according to user stories

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,41 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+
+  # A user can add a new bookmark (movie/list pair) to an existing list
+  # GET lists/42/bookmarks/new
+  # POST lists/42/bookmarks
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # A user can delete a bookmark from a list
+  # DELETE bookmarks/25
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:list).permit(:name)
+  end
 end

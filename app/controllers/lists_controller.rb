966f9753d4fb3e8d4ce141fad2e1cb41b038f8fd
Constanze Meyer <constanze.xyz@gmail.com>
2022-11-24 14:42:52 +0100

    add methods to lists_controller based on user stories

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,36 @@
 class ListsController < ApplicationController
+  # A user can see all the lists
+  # GET "lists"
+  def index
+    @lists = List.all
+  end
+
+  # A user can see the details of a given list and its name
+  # GET lists/42
+  def show
+    @list = List.find(params['id'])
+  end
+
+  # A user can create a new list
+  # GET "lists/new"
+  # POST "lists"
+  def new
+    @list = List.new(lists_params)
+
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @list = List.new
+  end
+
+  private
+
+  def lists_params
+    params.require(:list).permit(:name)
+  end
 end
